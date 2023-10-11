2ab0f4b69cb723da7809e0d43b94ae7c35f96c0c
Keiner De La Hoz <kfdelahoz@gmail.com>
2023-10-10 19:47:04 -0500

    view FormProduct y ShowProduct agregadas, view MainActivity completada

--- app/src/main/java/com/example/a2do_parcial_app_movil/MainActivity.kt
+++ app/src/main/java/com/example/a2do_parcial_app_movil/MainActivity.kt
@@ -10,4 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onAddButtonClick(view: View) {
+        // Crear un intent para iniciar la actividad FormMovie
+        val intent = Intent(this, FormProduct::class.java)
+        startActivity(intent)
+    }
+
+    // Método que se llama cuando se hace clic en el botón "Show"
+    fun onShowButtonClick(view: View) {
+        // Crear un intent para iniciar la actividad ShowMovies
+        val intent = Intent(this, ShowProducts::class.java)
+        startActivity(intent)
+    }
 }
---
+++ app/src/main/java/com/example/a2do_parcial_app_movil/ShowProducts.kt
@@ -0,0 +1,11 @@
+package com.example.a2do_parcial_app_movil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProducts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_products)
+    }
+}
