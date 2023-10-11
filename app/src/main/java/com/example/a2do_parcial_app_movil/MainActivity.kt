016e6efbc29ee6c5e331a6e3b06bab06888cc792
Keiner De La Hoz <kfdelahoz@gmail.com>
2023-10-10 19:36:37 -0500

    first commit

---
+++ app/src/main/java/com/example/a2do_parcial_app_movil/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a2do_parcial_app_movil
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
